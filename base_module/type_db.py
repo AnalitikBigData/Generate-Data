f9ab73879c274b6f5afbdbed242b1d57bcacd8e0
Veronika <morozznika@gmail.com>
2025-07-24 22:56:17 +0300

    каркас моделей для генератора данных

---
+++ base_module/type_db.py
@@ -0,0 +1,6 @@
+# словарь для сопоставления соединений к бд и типами бд
+dict_db_conn = {
+    "postgresql": "postgresql+psycopg2",
+    "mysql": "mysql+pymysql",
+    "oracle": "oracle+cx_oracle"
+}
